6e83383962615134716cc657204ad699f5e1b7a3
Yang Zhou <yangzhou@cs.umass.edu>
2018-10-07 15:44:15 -0400

    Update with public model annotation

--- maya_animation.py
+++ maya_animation.py
@@ -4,9 +4,15 @@ import maya.cmds as cmds
 test_audio_name = 'visemenet_intro'
 
 # change to absolute root directory if necessary
-f_pred = 'data/output_viseme/' + test_audio_name + '/mayaparam_viseme.txt'
+f_pred = '/Users/yangzhou/Documents/git/yzhou359/VisemeNet_tensorflow/data/output_viseme/' + test_audio_name + '/mayaparam_viseme.txt'
 
 params = ['Jaw', 'Lip', 'Ah', 'Aa', 'Eh', 'Ee', 'Ih', 'Oh', 'Uh', 'U', 'Eu', 'Schwa', 'R', 'S', 'Sh Ch Zh', 'Th', 'JY', 'LNTD', 'GK', 'MBP', 'FV', 'WA_PEDAL']
+
+param_public_model = ['translateX', 'translateY', 'AAA', 'Eh', 'AHH', 'OHH', 'UUU', 'IEE', 'RRR', 'WWW', 'SSS', 'FFF', 'TTH', 'MBP', 'SSH', 'Schwa', 'GK', 'LNTD', 'COARTIC.LNTD', 'COARTIC.GK', 'COARTIC.MMM', 'COARTIC.FFF', 'COARTIC.WA_PEDAL', 'COARTIC.YA_PEDAL']
+
+map_i = p_map = [0, 1, 3, 4, 2, [7, 8], 9, [5, 6], 12, 9, 13, 20, 15, 19, 14, 11, 18, 17, 17, 18, 19, 20, 21]
+
+
 pred = open(f_pred, 'r')
 y_pred = []
 for line in pred:
@@ -26,12 +32,19 @@ for sample_idx in range(0, len(y_pred), 4):
             sample[i] = 0
         elif sample[i] > 1:
             sample[i] = 1
-        if i in [0,1]:
-            cmds.setAttr("JaLi."+params[i], sample[i]*12)
-            cmds.setKeyframe("JaLi."+params[i])
-        elif i in range(2,19):
-            cmds.setAttr("CNT_PHONEMES."+params[i], sample[i]*10)
-            cmds.setKeyframe("CNT_PHONEMES."+params[i])
-        elif i in range(19,22):
-            cmds.setAttr("CNT_NOJAW."+params[i], sample[i]*10)
-            cmds.setKeyframe("CNT_NOJAW."+params[i])
+
+    for i in range(len(map_i)):
+        if i in [0, 1]:
+            cmds.setAttr("CNT_JaLi."+param_public_model[i], sample[map_i[i]]*10)
+            cmds.setKeyframe("CNT_JaLi."+param_public_model[i])
+
+        elif i in range(2, 18):
+            if(type(map_i[i]) == list):
+                tmp = max(sample[map_i[i][0]], sample[map_i[i][1]])
+            else:
+                tmp = sample[map_i[i]]
+            cmds.setAttr("CNT_PHONEMES."+param_public_model[i], tmp*10)
+            cmds.setKeyframe("CNT_PHONEMES."+param_public_model[i])
+        else:
+            cmds.setAttr("CNT_"+param_public_model[i], sample[map_i[i]]*10)
+            cmds.setKeyframe("CNT_"+param_public_model[i])
